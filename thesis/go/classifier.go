08eadbb480b3f2b324695df561ea2198bc7d3220
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-11-08 17:22:26 +0100

    Refresh classifier.go in the thesis' directory

--- thesis/go/classifier.go
+++ thesis/go/classifier.go
@@ -6,30 +6,47 @@ import (
 	"github.com/amwolff/be/apps/experimental/internal/levenshtein"
 )
 
-const sep = " "
+const (
+	Undefined = "f1a626ef-1003-44d8-9cff-f88019975c5c"
+	sep       = " "
+)
+
+func stringValueOrError(err, val *string) string {
+	if err != nil {
+		return *err
+	}
+	if val != nil {
+		return *val
+	}
+	return Undefined
+}
+
+func standardStringValueOrError(err *string, val string) string {
+	if err != nil {
+		return *err
+	}
+	return val
+}
 
 type OsCPU struct {
-	Value    string `json:"value"`
-	Error    string `json:"error"`
-	Duration int    `json:"duration"`
+	Value    *string `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
 }
 
 func (o OsCPU) String() string {
-	if len(o.Error) > 0 {
-		return o.Error
-	}
-	return o.Value
+	return stringValueOrError(o.Error, o.Value)
 }
 
 type Languages struct {
 	Value    [][]string `json:"value"`
-	Error    string     `json:"error"`
+	Error    *string    `json:"error"`
 	Duration int        `json:"duration"`
 }
 
 func (l Languages) String() string {
-	if len(l.Error) > 0 {
-		return l.Error
+	if l.Error != nil {
+		return *l.Error
 	}
 
 	var b strings.Builder
@@ -44,91 +61,94 @@ func (l Languages) String() string {
 	return b.String()
 }
 
-type (
-	ColorDepth struct {
-		Value    int    `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	DeviceMemory struct {
-		Value    int    `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	ScreenResolution struct {
-		Value    [2]int `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	AvailableScreenResolution struct {
-		Value    [2]int `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	HardwareConcurrency struct {
-		Value    int    `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	TimezoneOffset struct {
-		Value    int    `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-)
+type ColorDepth struct {
+	Value    int     `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type DeviceMemory struct {
+	Value    *int    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type ScreenResolution struct {
+	Value    [2]int  `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type AvailableScreenResolution struct {
+	Value    *[2]int `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type HardwareConcurrency struct {
+	Value    int     `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type TimezoneOffset struct {
+	Value    int     `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
 
 type Timezone struct {
-	Value    string `json:"value"`
-	Error    string `json:"error"`
-	Duration int    `json:"duration"`
+	Value    *string `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
 }
 
 func (t Timezone) String() string {
-	if len(t.Error) > 0 {
-		return t.Error
-	}
-	return t.Value
+	return stringValueOrError(t.Error, t.Value)
 }
 
-type (
-	SessionStorage struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	LocalStorage struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	IndexedDB struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	OpenDatabase struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	CPUClass struct {
-		Value    string `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-)
+type SessionStorage struct {
+	Value    bool    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type LocalStorage struct {
+	Value    bool    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type IndexedDB struct {
+	Value    *bool   `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type OpenDatabase struct {
+	Value    bool    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type CPUClass struct {
+	Value    *string `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+func (c CPUClass) String() string {
+	return stringValueOrError(c.Error, c.Value)
+}
 
 type Platform struct {
-	Value    string `json:"value"`
-	Error    string `json:"error"`
-	Duration int    `json:"duration"`
+	Value    string  `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
 }
 
 func (p Platform) String() string {
-	if len(p.Error) > 0 {
-		return p.Error
-	}
-	return p.Value
+	return standardStringValueOrError(p.Error, p.Value)
 }
 
 type (
@@ -137,20 +157,24 @@ type (
 		Suffixes string `json:"suffixes"`
 	}
 	PluginsValue struct {
-		Name        string   `json:"name"`
-		Description string   `json:"description"`
-		MimeTypes   []string `json:"mimeTypes"`
+		Name        string                  `json:"name"`
+		Description string                  `json:"description"`
+		MimeTypes   []PluginsValueMimeTypes `json:"mimeTypes"`
 	}
 	Plugins struct {
 		Value    []PluginsValue `json:"value"`
-		Error    string         `json:"error"`
+		Error    *string        `json:"error"`
 		Duration int            `json:"duration"`
 	}
 )
 
 func (p Plugins) String() string {
-	if len(p.Error) > 0 {
-		return p.Error
+	if p.Error != nil {
+		return *p.Error
+	}
+
+	if p.Value == nil {
+		return Undefined
 	}
 
 	var b strings.Builder
@@ -159,11 +183,17 @@ func (p Plugins) String() string {
 		if i > 0 {
 			b.WriteString(sep)
 		}
+
 		b.WriteString(v.Name)
 		b.WriteString(sep)
 		b.WriteString(v.Description)
-		b.WriteString(sep)
-		b.WriteString(strings.Join(v.MimeTypes, sep))
+
+		for _, t := range v.MimeTypes {
+			b.WriteString(sep)
+			b.WriteString(t.Type)
+			b.WriteString(sep)
+			b.WriteString(t.Suffixes)
+		}
 	}
 
 	return b.String()
@@ -176,9 +206,12 @@ type (
 	}
 	Canvas struct {
 		Value    CanvasValue `json:"value"`
-		Error    string      `json:"error"`
+		Error    *string     `json:"error"`
 		Duration int         `json:"duration"`
 	}
+)
+
+type (
 	TouchSupportValue struct {
 		MaxTouchPoints int  `json:"maxTouchPoints"`
 		TouchEvent     bool `json:"touchEvent"`
@@ -186,91 +219,82 @@ type (
 	}
 	TouchSupport struct {
 		Value    TouchSupportValue `json:"value"`
-		Error    string            `json:"error"`
+		Error    *string           `json:"error"`
 		Duration int               `json:"duration"`
 	}
 )
 
 type Fonts struct {
 	Value    []string `json:"value"`
-	Error    string   `json:"error"`
+	Error    *string  `json:"error"`
 	Duration int      `json:"duration"`
 }
 
 func (f Fonts) String() string {
-	if len(f.Error) > 0 {
-		return f.Error
+	if f.Error != nil {
+		return *f.Error
 	}
 	return strings.Join(f.Value, sep)
 }
 
-type (
-	Audio struct {
-		Value    float64 `json:"value"`
-		Error    string  `json:"error"`
-		Duration int     `json:"duration"`
-	}
-	PluginsSupport struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-)
+type Audio struct {
+	Value    float64 `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type PluginsSupport struct {
+	Value    bool    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
 
 type ProductSub struct {
-	Value    string `json:"value"`
-	Error    string `json:"error"`
-	Duration int    `json:"duration"`
+	Value    string  `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
 }
 
 func (p ProductSub) String() string {
-	if len(p.Error) > 0 {
-		return p.Error
-	}
-	return p.Value
+	return standardStringValueOrError(p.Error, p.Value)
 }
 
-type (
-	EmptyEvalLength struct {
-		Value    int    `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	ErrorFF struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-)
+type EmptyEvalLength struct {
+	Value    int     `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type ErrorFF struct {
+	Value    bool    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
 
 type Vendor struct {
-	Value    string `json:"value"`
-	Error    string `json:"error"`
-	Duration int    `json:"duration"`
+	Value    string  `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
 }
 
 func (v Vendor) String() string {
-	if len(v.Error) > 0 {
-		return v.Error
-	}
-	return v.Value
+	return standardStringValueOrError(v.Error, v.Value)
 }
 
-type (
-	Chrome struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-	CookiesEnabled struct {
-		Value    bool   `json:"value"`
-		Error    string `json:"error"`
-		Duration int    `json:"duration"`
-	}
-)
+type Chrome struct {
+	Value    bool    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
+
+type CookiesEnabled struct {
+	Value    bool    `json:"value"`
+	Error    *string `json:"error"`
+	Duration int     `json:"duration"`
+}
 
 type Fingerprint struct {
-	VisitorID                 string                    `json:"-"`
+	VisitorID                 string                    `json:"visitorID"`
 	OsCPU                     OsCPU                     `json:"osCpu"`
 	Languages                 Languages                 `json:"languages"`
 	ColorDepth                ColorDepth                `json:"colorDepth"`
@@ -311,7 +335,176 @@ func (w Worthless) Do(f Fingerprint) (Fingerprint, bool) {
 }
 
 func hardwareRelatedCompatibility(a, b Fingerprint) float64 {
-	return 0.9
+	var count float64
+
+	if a.ColorDepth.Error == nil && b.ColorDepth.Error == nil &&
+		a.ColorDepth.Value == b.ColorDepth.Value {
+		count++
+	} else if a.ColorDepth.Error != nil && b.ColorDepth.Error != nil &&
+		*a.ColorDepth.Error == *b.ColorDepth.Error {
+		count++
+	}
+
+	if a.DeviceMemory.Error == nil && b.DeviceMemory.Error == nil {
+		if a.DeviceMemory.Value == nil && b.DeviceMemory.Value == nil {
+			count++
+		} else if a.DeviceMemory.Value != nil && b.DeviceMemory.Value != nil &&
+			*a.DeviceMemory.Value == *b.DeviceMemory.Value {
+			count++
+		}
+	} else if a.DeviceMemory.Error != nil && b.DeviceMemory.Error != nil &&
+		*a.DeviceMemory.Error == *b.DeviceMemory.Error {
+		count++
+	}
+
+	if a.ScreenResolution.Error == nil && b.ScreenResolution.Error == nil &&
+		a.ScreenResolution.Value == b.ScreenResolution.Value {
+		count++
+	} else if a.ScreenResolution.Error != nil && b.ScreenResolution.Error != nil &&
+		*a.ScreenResolution.Error == *b.ScreenResolution.Error {
+		count++
+	}
+
+	if a.AvailableScreenResolution.Error == nil && b.AvailableScreenResolution.Error == nil {
+		if a.AvailableScreenResolution.Value == nil && b.AvailableScreenResolution.Value == nil {
+			count++
+		} else if a.AvailableScreenResolution.Value != nil && b.AvailableScreenResolution.Value != nil &&
+			*a.AvailableScreenResolution.Value == *b.AvailableScreenResolution.Value {
+			count++
+		}
+	} else if a.AvailableScreenResolution.Error != nil && b.AvailableScreenResolution.Error != nil &&
+		*a.AvailableScreenResolution.Error == *b.AvailableScreenResolution.Error {
+		count++
+	}
+
+	if a.HardwareConcurrency.Error == nil && b.HardwareConcurrency.Error == nil &&
+		a.HardwareConcurrency.Value == b.HardwareConcurrency.Value {
+		count++
+	} else if a.HardwareConcurrency.Error != nil && b.HardwareConcurrency.Error != nil &&
+		*a.HardwareConcurrency.Error == *b.HardwareConcurrency.Error {
+		count++
+	}
+
+	if a.TimezoneOffset.Error == nil && b.TimezoneOffset.Error == nil &&
+		a.TimezoneOffset.Value == b.TimezoneOffset.Value {
+		count++
+	} else if a.TimezoneOffset.Error != nil && b.TimezoneOffset.Error != nil &&
+		*a.TimezoneOffset.Error == *b.TimezoneOffset.Error {
+		count++
+	}
+
+	if a.SessionStorage.Error == nil && b.SessionStorage.Error == nil &&
+		a.SessionStorage.Value == b.SessionStorage.Value {
+		count++
+	} else if a.SessionStorage.Error != nil && b.SessionStorage.Error != nil &&
+		*a.SessionStorage.Error == *b.SessionStorage.Error {
+		count++
+	}
+
+	if a.LocalStorage.Error == nil && b.LocalStorage.Error == nil &&
+		a.LocalStorage.Value == b.LocalStorage.Value {
+		count++
+	} else if a.LocalStorage.Error != nil && b.LocalStorage.Error != nil &&
+		*a.LocalStorage.Error == *b.LocalStorage.Error {
+		count++
+	}
+
+	if a.IndexedDB.Error == nil && b.IndexedDB.Error == nil {
+		if a.IndexedDB.Value == nil && b.IndexedDB.Value == nil {
+			count++
+		} else if a.IndexedDB.Value != nil && b.IndexedDB.Value != nil &&
+			*a.IndexedDB.Value == *b.IndexedDB.Value {
+			count++
+		}
+	} else if a.IndexedDB.Error != nil && b.IndexedDB.Error != nil &&
+		*a.IndexedDB.Error == *b.IndexedDB.Error {
+		count++
+	}
+
+	if a.OpenDatabase.Error == nil && b.OpenDatabase.Error == nil &&
+		a.OpenDatabase.Value == b.OpenDatabase.Value {
+		count++
+	} else if a.OpenDatabase.Error != nil && b.OpenDatabase.Error != nil &&
+		*a.OpenDatabase.Error == *b.OpenDatabase.Error {
+		count++
+	}
+
+	if a.Canvas.Error == nil && b.Canvas.Error == nil {
+		if a.Canvas.Value.Winding == b.Canvas.Value.Winding {
+			count++
+		}
+		if a.Canvas.Value.Data == b.Canvas.Value.Data {
+			count++
+		}
+	} else if a.Canvas.Error != nil && b.Canvas.Error != nil &&
+		*a.Canvas.Error == *b.Canvas.Error {
+		count += 2
+	}
+
+	if a.TouchSupport.Error == nil && b.TouchSupport.Error == nil {
+		if a.TouchSupport.Value.MaxTouchPoints == b.TouchSupport.Value.MaxTouchPoints {
+			count++
+		}
+		if a.TouchSupport.Value.TouchEvent == b.TouchSupport.Value.TouchEvent {
+			count++
+		}
+		if a.TouchSupport.Value.TouchStart == b.TouchSupport.Value.TouchStart {
+			count++
+		}
+	} else if a.TouchSupport.Error != nil && b.TouchSupport.Error != nil &&
+		*a.TouchSupport.Error == *b.TouchSupport.Error {
+		count += 3
+	}
+
+	if a.Audio.Error == nil && b.Audio.Error == nil &&
+		a.Audio.Value == b.Audio.Value {
+		count++
+	} else if a.Audio.Error != nil && b.Audio.Error != nil &&
+		*a.Audio.Error == *b.Audio.Error {
+		count++
+	}
+
+	if a.PluginsSupport.Error == nil && b.PluginsSupport.Error == nil &&
+		a.PluginsSupport.Value == b.PluginsSupport.Value {
+		count++
+	} else if a.PluginsSupport.Error != nil && b.PluginsSupport.Error != nil &&
+		*a.PluginsSupport.Error == *b.PluginsSupport.Error {
+		count++
+	}
+
+	if a.EmptyEvalLength.Error == nil && b.EmptyEvalLength.Error == nil &&
+		a.EmptyEvalLength.Value == b.EmptyEvalLength.Value {
+		count++
+	} else if a.EmptyEvalLength.Error != nil && b.EmptyEvalLength.Error != nil &&
+		*a.EmptyEvalLength.Error == *b.EmptyEvalLength.Error {
+		count++
+	}
+
+	if a.ErrorFF.Error == nil && b.ErrorFF.Error == nil &&
+		a.ErrorFF.Value == b.ErrorFF.Value {
+		count++
+	} else if a.ErrorFF.Error != nil && b.ErrorFF.Error != nil &&
+		*a.ErrorFF.Error == *b.ErrorFF.Error {
+		count++
+	}
+
+	if a.Chrome.Error == nil && b.Chrome.Error == nil &&
+		a.Chrome.Value == b.Chrome.Value {
+		count++
+	} else if a.Chrome.Error != nil && b.Chrome.Error != nil &&
+		*a.Chrome.Error == *b.Chrome.Error {
+		count++
+	}
+
+	if a.CookiesEnabled.Error == nil && b.CookiesEnabled.Error == nil &&
+		a.CookiesEnabled.Value == b.CookiesEnabled.Value {
+		count++
+	} else if a.CookiesEnabled.Error != nil && b.CookiesEnabled.Error != nil &&
+		*a.CookiesEnabled.Error == *b.CookiesEnabled.Error {
+		count++
+	}
+
+	return count / 21
 }
 
 func max(a, b int) int {
@@ -334,6 +527,10 @@ func similarity(a, b Fingerprint) float64 {
 		{
 			a.Timezone.String(),
 			b.Timezone.String(),
+		},
+		{
+			a.CPUClass.String(),
+			b.CPUClass.String(),
 		},
 		{
 			a.Platform.String(),
@@ -360,6 +557,12 @@ func similarity(a, b Fingerprint) float64 {
 	var ratiosSum float64
 
 	for _, c := range components {
+		if c[0] == Undefined || c[1] == Undefined {
+			if c[0] == Undefined && c[1] == Undefined {
+				ratiosSum++
+			}
+			continue
+		}
 		d, l := levenshtein.LinearSpace(c[0], c[1]), max(len(c[0]), len(c[1]))
 		// We are subtracting from 1 here because it then means that these two
 		// strings are compatible in this ratio.
@@ -383,7 +586,7 @@ func (e ExperimentalInMemory) Do(f Fingerprint) (Fingerprint, bool) {
 
 	for _, fingerprints := range e.Fingerprints {
 		for _, g := range fingerprints {
-			if hardwareRelatedCompatibility(f, g) < 0.9 {
+			if hardwareRelatedCompatibility(f, g) < 0.85 {
 				continue
 			}
 
@@ -393,7 +596,7 @@ func (e ExperimentalInMemory) Do(f Fingerprint) (Fingerprint, bool) {
 		}
 	}
 
-	if max > 0.8 {
+	if max > 0.9 {
 		return candidate, true
 	}
 
