03403ef76540955907e3c0538ab8f653939fca0c
Artur M. Wolff <artur.m.wolff@gmail.com>
2020-11-09 21:04:23 +0100

    Finish the experimental web app

--- apps/experimental/cmd/server/main.go
+++ apps/experimental/cmd/server/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -12,18 +11,22 @@ import (
 	"go.uber.org/zap"
 )
 
+func logRequest(sugar *zap.SugaredLogger, r *http.Request) {
+	b, err := httputil.DumpRequest(r, true)
+	if err != nil {
+		sugar.Debugf("DumpRequest: %v", err)
+	} else {
+		sugar.Debugf("\n%s", strings.TrimSpace(string(b)))
+	}
+}
+
 func getRootHandler(sugar *zap.SugaredLogger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		b, err := httputil.DumpRequest(r.Clone(context.TODO()), true)
-		if err != nil {
-			sugar.Debugf("DumpRequest: %v", err)
-		} else {
-			sugar.Debugf("\n%s", strings.TrimSpace(string(b)))
-		}
+		logRequest(sugar, r)
 
-		http.Error(w, "", http.StatusBadRequest)
+		http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 	}
 }
 
@@ -31,51 +34,48 @@ func getFpHandler(sugar *zap.SugaredLogger, c classifier.Classifier) http.Handle
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		b, err := httputil.DumpRequest(r, false)
-		if err != nil {
-			sugar.Debugf("DumpRequest: %v", err)
-		} else {
-			sugar.Debugf("\n%s", strings.TrimSpace(string(b)))
-		}
+		logRequest(sugar, r)
 
 		if r.Method != http.MethodPost {
-			http.Error(w, "", http.StatusBadRequest)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 			return
 		}
+
+		d := json.NewDecoder(r.Body)
+		d.DisallowUnknownFields()
 
 		var f classifier.Fingerprint
 
-		if err := json.NewDecoder(r.Body).Decode(&f); err != nil {
+		if err := d.Decode(&f); err != nil {
 			sugar.Errorf("NewDecoder: %v", err)
-			http.Error(w, "", http.StatusBadRequest)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
 			return
 		}
+
+		var entropy float64
 
 		sf, ok := c.Do(f)
 		if !ok {
 			sugar.Infow("Got new", "f", f.VisitorID)
 			sugar.Debug(f)
 
-			e := c.Store(f.VisitorID, f)
+			entropy = c.Store(f.VisitorID, f)
 
-			sugar.Infof("The set has now %.2f bits of entropy", e)
+			http.Error(w, http.StatusText(http.StatusCreated), http.StatusCreated)
+		} else {
+			sugar.Infow("Got similar", "f", f.VisitorID, "similar", sf.VisitorID)
+			sugar.Debug(f)
+			sugar.Debug(sf)
 
-			http.Error(w, "", http.StatusCreated)
-			return
+			entropy = c.Store(sf.VisitorID, f)
 		}
 
-		sugar.Infow("Got similar", "f", f.VisitorID, "similar", sf.VisitorID)
-		sugar.Debug(f)
-		sugar.Debug(sf)
-
-		e := c.Store(sf.VisitorID, f)
-
-		sugar.Infof("The set has now %.2f bits of entropy", e)
+		sugar.Infof("The set has now %.2f bits of entropy", entropy)
 	}
 }
 
 func main() {
-	l, err := zap.NewDevelopment()
+	l, err := zap.NewDevelopment(zap.IncreaseLevel(zap.InfoLevel))
 	if err != nil {
 		panic(fmt.Sprintf("NewProduction: %v", err))
 	}
--- apps/experimental/web/app/index.js
+++ apps/experimental/web/app/index.js
@@ -1,10 +1,72 @@
+'use strict';
+
 import 'regenerator-runtime/runtime';
 import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
+const url = 'http://192.168.0.172:8084/fp';
+
+function addOutput(node, text, size) {
+    const p = document.createElement('pre');
+
+    p.classList.add(size);
+    p.textContent = text;
+
+    node.appendChild(p);
+}
+
+function addHeader(node, text) {
+    const h = document.createElement('div');
+
+    h.classList.add('heading');
+    h.textContent = text;
+
+    node.appendChild(h);
+}
+
+function addSection(output, header, content, size) {
+    addHeader(output, header);
+    addOutput(output, content, size);
+}
+
 (async () => {
+    const t0 = performance.now();
+
     const fp = await FingerprintJS.load();
-
     const result = await fp.get();
+    const components = result.components;
 
-    console.log(result.visitorId);
+    components['visitorID'] = result.visitorId;
+
+    const options = {
+        method: 'POST',
+        body: JSON.stringify(components),
+        // headers: {
+        //     'Content-Type': 'text/plain'
+        // }
+    };
+
+    const response = await fetch(url, options);
+
+    const t1 = performance.now();
+
+    document.querySelector('#status').textContent = 'Fingerprinting... 100%';
+
+    const output = document.querySelector('.output');
+    let content;
+
+    switch (response.status) {
+        case 200:
+            content = 'Your browser/device already exists in the database.';
+            break;
+        case 201:
+            content = "Your browser/device didn't exist in the database before";
+            break;
+        default:
+            return;
+    }
+
+    addSection(output, 'Status', content, 'small');
+    addSection(output, 'ID', result.visitorId, 'big');
+    addSection(output, 't_1 - t_0', t1 - t0 + 'ms', 'small');
+    addSection(output, 'Components', JSON.stringify(result.components, null, 2), 'small');
 })();
